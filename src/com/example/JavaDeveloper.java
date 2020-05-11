58a0828203806610cb10df555425aaf3080fd6ca
Daria Tsvetkova <dasha21a@icloud.com>
2020-05-11 19:38:29 +0300

    add Woman.java

---
+++ src/com/example/JavaDeveloper.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class JavaDeveloper implements Developer{
+    @Override
+    public void writeCode() {
+        System.out.println("java Developer is writing Java Code...");
+    }
+}
