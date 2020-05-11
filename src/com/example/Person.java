95c1b4714d62bd45c4d43719f80345b374cf98ca
Daria Tsvetkova <dasha21a@icloud.com>
2020-05-11 19:28:49 +0300

    add Person.java

---
+++ src/com/example/Person.java
@@ -0,0 +1,31 @@
+package com.example;
+
+public class Person extends BaseEntity{
+    private String firstName;
+    private String surname;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + super.getId() + '\'' +
+                "id='" + firstName + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+}
